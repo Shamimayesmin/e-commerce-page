6427cef84d500925d1070863404279a97e22d345
shamima <shamimayesmin144@gamil.com>
2023-03-10 16:47:49 +0600

    orders added

--- src/components/pages/OverView/OverView.js
+++ src/components/pages/OverView/OverView.js
@@ -1,15 +1,15 @@
 import React from "react";
 import Chart from "./Chart";
 import MonthIncome from "./MonthIncome";
-//mx-4
+//mx-4 flex-col lg:flex-row-reverse    shadow-lg rounded-lg mt-4 bg-white container w-full flex-col items-center justify-center lg:flex-row lg:flex-wrap lg:justify-evenly lg:px-10 sm:justify-between
 const OverView = () => {
 	return (
 		<div>
-			<div class="flex shadow-lg rounded-lg mt-4 bg-white container w-full flex-col items-center justify-center lg:flex-row lg:flex-wrap lg:justify-evenly lg:px-10 sm:justify-between">
-				<div class="w-2/5 container mx-auto">
+			<div class=" flex flex-col lg:flex-row">
+				<div class="lg:w-2/5 sm:w-full md:w-full rounded-lg container mx-auto">
                    <MonthIncome></MonthIncome>
                 </div>
-				<div class="w-3/5">
+				<div class="lg:w-3/5 sm:w-full md:w-full rounded-lg">
                    <Chart></Chart>
                 </div>
 			</div>
